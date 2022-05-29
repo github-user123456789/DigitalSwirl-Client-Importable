bd2bd8e40b1268ceb8d518117c82f09e18de3414
github-user123456789 <99767746+github-user123456789@users.noreply.github.com>
2022-05-29 17:45:00 +0100

    Update Ring.lua

--- ControlScript/Object/Ring.lua
+++ ControlScript/Object/Ring.lua
@@ -1,171 +1,101 @@
 --[[
+	= Sonic Onset Adventure Client =
+	Source: ControlScript/Object/DashRing.lua
+	Purpose: Dash Ring object
+	Author(s): Regan "CuckyDev/TheGreenDeveloper" Green
+	--]]
 
-= Sonic Onset Adventure Client =
+	local dash_ring = {}
 
-Source: ControlScript/Object/Ring.lua
-Purpose: Ring object
-Author(s): Regan "CuckyDev/TheGreenDeveloper" Green
+	local assets = GLOBALASSETS
+	local obj_assets = assets:WaitForChild("DashRing")
 
---]]
+	local replicated_storage = game:GetService("ReplicatedStorage")
+	local common_modules = commons
 
-local ring = {}
+	local cframe = require(common_modules.CFrame)
+	local vector = require(common_modules.Vector)
+	local constants = require(pcontrol.Constants)
 
-local assets = script.Parent.Parent:WaitForChild("Assets")
-local obj_assets = assets:WaitForChild("Ring")
-
---Object functions
-local function Update(self, i)
-	if self.collected then
-		--Restore position once collect animation is over
-		if self.draw == nil then
-			self.object:SetPrimaryPartCFrame(self.og_cf)
+	--Object functions
+	local function Update(self, i)
+		--Decrement debounce
+		if self.debounce > 0 then
+			self.debounce = math.max(self.debounce - 1, 0)
+		else
 			self.update = nil
 		end
-	elseif self.attract_player ~= nil then
-		--Adjust speed
-		local diff = self.attract_player:GetMiddle() - self.root.Position
-		if diff.magnitude ~= 0 then
-			local spd_accel = (self.spd * (0.925 + (self.attract_force * (1 - 0.925)))) + (diff.unit * 0.175 * self.attract_force)
-			self.spd = spd_accel:Lerp(diff, self.attract_force)
+	end
+
+	--Object contact
+	local function TouchPlayer(self, player)
+		--Disable dash panels for SEO v3
+		if player.v3 then
+			return
 		end
 		
-		--Increase attraction
-		self.attract_force = math.min(self.attract_force + 0.005, 1)
+		--Perform debounce check
+		if self.debounce == nil or self.debounce <= 0 then
+			--Move player to rainbow ring and set speed
+			player:SetAngle(player:AngleFromRbx(self.root.CFrame - self.root.CFrame.p))
+			player.pos = self.root.Position - (player:GetUp() * (player.p.height * player.p.scale))
+			player.spd = Vector3.new((self.power / 60) / player.p.scale, 0, 0)
+			
+			--Set dash ring state and make airborne
+			player.state = constants.state.airborne
+			player.flag.grounded = false
+			player:ExitBall()
+			player:ResetObjectState()
+			player.dashring_timer = self.nocon_time * 60
+			player.animation = "DashRing"
+			player.reset_anim = true
+			
+			--Play touch sound
+			self.touch_sound:Play()
+			
+			--Set debounce
+			self.debounce = 6
+			self.update = Update
+		end
+	end
+
+	--Object functions
+	local function Update(self, i)
+		--Decrement debounce
+		if self.debounce == nil then
+			self.debounce = 0
+		elseif self.debounce > 0 then
+			self.debounce = math.max(self.debounce - 1, 0)
+		end
+	end
+
+	--Constructor and destructor
+	function dash_ring:New(object)
+		--Initialize meta reference
+		local self = setmetatable({}, {__index = dash_ring})
 		
-		--Move
-		self.object:SetPrimaryPartCFrame(self.root.CFrame + self.spd)
+		--Use object information
+		self.object = object
+		self.root = object.PrimaryPart
+		self.nocon_time = self.object:WaitForChild("Nocon").Value
+		self.power = self.object:WaitForChild("Power").Value
+		
+		--Create touch sound
+		self.touch_sound = obj_assets:WaitForChild("TouchSound"):clone()
+		self.touch_sound.Parent = self.root
+		
+		--Attach functions
+		self.touch_player = TouchPlayer
+		
+		return self
 	end
-end
 
-local function Draw(self, dt)
-	local done = true
-	
-	--Fade light
-	if self.light.Brightness > 0 then
-		self.light.Brightness = math.max(self.light.Brightness - (dt / 0.5), 0)
-		done = false
-	end
-	
-	--Destroy particle once lifetime is over
-	if self.touch_particle ~= nil then
-		self.touch_particle_life -= dt
-		if self.touch_particle_life <= 0 then
-			self.touch_particle:Destroy()
-			self.touch_particle = nil
-		else
-			done = false
+	function dash_ring:Destroy()
+		--Destroy sound
+		if self.touch_sound ~= nil then
+			self.touch_sound:Destroy()
+			self.touch_sound = nil
 		end
 	end
-	
-	--Stop running once done
-	if done then
-		self.draw = nil
-	end
-end
 
---Object contact
-local function TouchPlayer(self, player)
-	if player.v3 then
-		return
-	end
-	
-	--Change object state
-	self.draw = Draw
-	self.touch_player = nil
-	
-	--Give player ring and collect
-	player:GiveScore(10)
-	player:GiveRings(1)
-	self.collected = true
-	
-	--Hide ring and destroy animation
-	self.ring.LocalTransparencyModifier = 1
-	if self.anim ~= nil then
-		self.anim:Destroy()
-		self.anim = nil
-	end
-	
-	--Particle and sound
-	if self.touch_particle ~= nil then
-		self.touch_particle:Emit(20)
-	end
-	self.touch_sound:Play()
-end
-
---Constructor and destructor
-function ring:New(object)
-	--Initialize meta reference
-	local self = setmetatable({}, {__index = ring})
-	
-	--Use object information
-	self.object = object
-	self.root = object.PrimaryPart
-	self.ring = object:WaitForChild("Ring")
-	self.light = self.ring:WaitForChild("PointLight")
-	self.light_brightness = self.light.Brightness
-	self.anim_controller = object:WaitForChild("AnimationController")
-	
-	--Remember ring object position
-	self.og_cf = self.ring.CFrame
-	
-	--Create touch sound and particle
-	self.touch_sound = obj_assets:WaitForChild("TouchSound"):clone()
-	self.touch_sound.Parent = self.root
-	self.particle_attachment = Instance.new("Attachment", self.root)
-	self.touch_particle = obj_assets:WaitForChild("TouchParticle"):clone()
-	self.touch_particle.Parent = self.particle_attachment
-	self.touch_particle_life = self.touch_particle.Lifetime.Max
-	
-	--Create and play animation
-	self.anim = self.anim_controller:LoadAnimation(obj_assets:WaitForChild("Anim"))
-	self.anim:Play()
-	
-	--Attach functions
-	self.touch_player = TouchPlayer
-	
-	--Set state
-	self.spd = Vector3.new()
-	self.attract_force = 0
-	self.attract_player = nil
-	self.collected = false
-	
-	return self
-end
-
-function ring:Destroy()
-	--Restore ring visibility and light
-	if self.collected then
-		self.ring.LocalTransparencyModifier = 0
-		if self.light ~= nil then
-			self.light.Brightness = self.light_brightness
-		end
-	end
-	
-	--Destroy animation
-	if self.anim ~= nil then
-		self.anim:Destroy()
-		self.anim = nil
-	end
-	
-	--Destroy sound and particles
-	if self.touch_sound ~= nil then
-		self.touch_sound:Destroy()
-		self.touch_sound = nil
-	end
-	if self.particle_attachment ~= nil then
-		self.particle_attachment:Destroy()
-		self.particle_attachment = nil
-	end
-	
-	--Restore ring position
-	if self.update ~= nil then
-		self.object:SetPrimaryPartCFrame(self.og_cf)
-	end
-end
-
-function ring:Attract(player)
-	self.attract_player = player
-	self.update = Update
-end
-
-return ring
+	return dash_ring
