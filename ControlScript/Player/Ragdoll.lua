19f2ea9ac569b515ba9beec1a5d3f762e9364d5a
github-user123456789 <99767746+github-user123456789@users.noreply.github.com>
2022-05-29 15:03:48 +0100

    Update Ragdoll.lua

--- ControlScript/Player/Ragdoll.lua
+++ ControlScript/Player/Ragdoll.lua
@@ -1,61 +1,61 @@
 --[[
 
-= Sonic Onset Adventure Client =
+	= Sonic Onset Adventure Client =
 
-Source: ControlScript/Player/Ragdoll.lua
-Purpose: Player ragdoll functions
-Author(s): Regan "CuckyDev/TheGreenDeveloper" Green
+	Source: ControlScript/Player/Ragdoll.lua
+	Purpose: Player ragdoll functions
+	Author(s): Regan "CuckyDev/TheGreenDeveloper" Green
 
---]]
+	--]]
 
-local player_ragdoll = {}
+	local player_ragdoll = {}
 
-local constants = require(script.Parent.Parent:WaitForChild("Constants"))
+	local constants = require(pcontrol.Constants)
 
-local sound = require(script.Parent:WaitForChild("Sound"))
+	local sound = require(pcontrol.Player.Sound)
 
-function player_ragdoll.Bounce(self, pspd, nspd)
-	--Bounce
-	local diff = nspd - pspd
-	self.spd = self:ToLocal(nspd + diff * (0.5 + math.random() * 0.35))
-	
-	--Enter ragdoll if bounced hard
-	if diff.magnitude > 6 and self.state ~= constants.state.ragdoll then
-		--Enter ragdoll state
-		self.do_ragdoll = true--self.state = constants.state.ragdoll
-		self.ragdoll_time = 0
-		sound.PlaySound(self, "Trip")
-	end
-	
-	if diff.magnitude > 1 then
-		--Angle speed
-		local ang_force = self.spd.magnitude / 20
-		self.ragdoll_ang_spd = CFrame.Angles((math.random() * 2 - 1) * ang_force, (math.random() * 2 - 1) * ang_force, (math.random() * 2 - 1) * ang_force)
-	end
-end
-
-function player_ragdoll.Physics(self)
-	--Gravity and air drag
-	self.spd += self:ToLocal(self.gravity) * self.p.weight
-	self.spd *= 0.995
-	
-	--Rotate
-	local gspd = self:ToGlobal(self.spd)
-	self.flag.grounded = false
-	self:SetAngle(self.ang * self.ragdoll_ang_spd)
-	self.spd = self:ToLocal(gspd)
-	
-	--Check if we should stop
-	if self.spd.magnitude < 1 then
-		self.ragdoll_time += 1
-		if self.ragdoll_time > 60 then
-			sound.PlaySound(self, "GetUp")
-			return true
+	function player_ragdoll.Bounce(self, pspd, nspd)
+		--Bounce
+		local diff = nspd - pspd
+		self.spd = self:ToLocal(nspd + diff * (0.5 + math.random() * 0.35))
+		
+		--Enter ragdoll if bounced hard
+		if diff.magnitude > 6 and self.state ~= constants.state.ragdoll then
+			--Enter ragdoll state
+			self.do_ragdoll = true--self.state = constants.state.ragdoll
+			self.ragdoll_time = 0
+			sound.PlaySound(self, "Trip")
 		end
-	else
-		self.ragdoll_time = math.max(self.ragdoll_time - 1, 0)
+		
+		if diff.magnitude > 1 then
+			--Angle speed
+			local ang_force = self.spd.magnitude / 20
+			self.ragdoll_ang_spd = CFrame.Angles((math.random() * 2 - 1) * ang_force, (math.random() * 2 - 1) * ang_force, (math.random() * 2 - 1) * ang_force)
+		end
 	end
-	return false
-end
 
-return player_ragdoll
+	function player_ragdoll.Physics(self)
+		--Gravity and air drag
+		self.spd = self.spd + self:ToLocal(self.gravity) * self.p.weight
+		self.spd = self.spd * 0.995
+		
+		--Rotate
+		local gspd = self:ToGlobal(self.spd)
+		self.flag.grounded = false
+		self:SetAngle(self.ang * self.ragdoll_ang_spd)
+		self.spd = self:ToLocal(gspd)
+		
+		--Check if we should stop
+		if self.spd.magnitude < 1 then
+			self.ragdoll_time = self.ragdoll_time + 1
+			if self.ragdoll_time > 60 then
+				sound.PlaySound(self, "GetUp")
+				return true
+			end
+		else
+			self.ragdoll_time = math.max(self.ragdoll_time - 1, 0)
+		end
+		return false
+	end
+
+	return player_ragdoll
